eb9691e04ed5889930f783313b1458cc52298314
xiaoyueyue1001 <30659345+xiaoyueyue1001@users.noreply.github.com>
2022-04-29 11:52:29 +0800

    Create Loader.vue

---
+++ src/components/Loader.vue
@@ -0,0 +1,21 @@
+<script>
+import {h} from "vue";
+export default {
+    name: "vNode-loader",
+    props: {
+        vNode: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    setup(props){
+        return ()=> h(props.vNode);
+    }
+}
+</script>
+
+
+<style lang="scss">
+</style>
